d52c720c3d8c0e75e32f68f91a587b69fcdaace8
qkdkran123 <qkdkran123@gmail.com>
2024-02-28 23:59:33 +0900

    수정

--- projects/cyber_cesco/ai/__init__.py
+++ projects/cyber_cesco/ai/__init__.py
@@ -1,9 +1,9 @@
 import os
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras import Sequential
+from keras.src.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.src.preprocessing.image import ImageDataGenerator
 
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
